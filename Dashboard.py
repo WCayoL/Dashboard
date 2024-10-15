d966679283c52af0e45eb54a86b22f80ee000314
WCayoL <wilson.cayo@usach.cl>
2024-10-15 04:37:19 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 data_url = (r"C:\Users\Widdo\Desktop\Dashboard\IDP SEPTIEMBRE PRUEBA.csv")
 
-st.title("Siniestros de transito en Chile (2000 - 2023) 💥🚗")
-st.markdown("Datos obtenidos directamente de los archivos del gobierno.")
+st.title("Dashboard Retailatam 📈")
+st.markdown("")
