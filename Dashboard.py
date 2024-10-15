c8711fde70ed66896b7cccdc577fa87084eaa7f4
WCayoL <wilson.cayo@usach.cl>
2024-10-15 04:41:52 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -5,3 +5,16 @@ data_url = (r"C:\Users\Widdo\Desktop\Dashboard\IDP SEPTIEMBRE PRUEBA.csv")
 
 st.title("Dashboard Retailatam 📈")
 st.markdown("")
+
+@st.cache_data
+def load_data(nrows):
+   data = pd.read_csv(data_url, nrows=nrows)
+   #data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+   #lowercase = lambda x: str(x).lower()
+   #data.rename(lowercase, axis='columns', inplace=True)
+   #data.rename(columns={'crash_date_crash_time': "date/time"}, inplace=True)
+   return data
+
+data= load_data(100000)
+st.subheader('Raw Data')
+st.write(data)
