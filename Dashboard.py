a7fb66b3a31bacc6f5adcf8e3181ba87208d5b6e
WCayoL <wilson.cayo@usach.cl>
2024-10-15 04:58:51 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-data_url = ("IDP SEPTIEMBRE PRUEBA.csv")
+data_url = (r"C:\Users\Widdo\Desktop\Dashboard\IDP SEPTIEMBRE PRUEBA.csv")
 
 st.title("Dashboard Retailatam 📈")
 st.markdown("")
@@ -9,10 +9,6 @@ st.markdown("")
 @st.cache_data
 def load_data(nrows):
    data = pd.read_csv(data_url, nrows=nrows)
-   #data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-   #lowercase = lambda x: str(x).lower()
-   #data.rename(lowercase, axis='columns', inplace=True)
-   #data.rename(columns={'crash_date_crash_time': "date/time"}, inplace=True)
    return data
 
 data= load_data(100)
