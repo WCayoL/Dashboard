cd56bca5c326e267259d7e83800337a26c13f496
WCayoL <wilson.cayo@usach.cl>
2024-10-15 05:10:02 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -7,10 +7,10 @@ st.title("Dashboard Retailatam 📈")
 st.markdown("")
 
 @st.cache_data
-def load_data(nrows):
-   data = pd.read_csv(data_url, nrows=nrows, delimiter=';')
+def load_data():
+   data = pd.read_csv(data_url, delimiter=';')
    return data
 
-data= load_data(100)
+data= load_data()
 st.subheader('Raw Data')
 st.write(data)
