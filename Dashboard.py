aa62cf9e171444686941e68f390862e360c74b76
WCayoL <wilson.cayo@usach.cl>
2024-10-15 04:48:10 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-data_url = (r"C:\Users\Widdo\Desktop\Dashboard\IDP SEPTIEMBRE PRUEBA.csv")
+data_url = ("IDP SEPTIEMBRE PRUEBA.csv")
 
 st.title("Dashboard Retailatam 📈")
 st.markdown("")
